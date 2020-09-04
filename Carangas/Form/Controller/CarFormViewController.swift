619451213246ba469929418b67318c19cc9aebf5
Eric Brito <eric_brito@msn.com>
2020-09-04 19:04:03 -0300

    - Start

---
+++ Carangas/Form/Controller/CarFormViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddEditViewController.swift
+//  Carangas
+//
+//  Copyright © 2020 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class CarFormViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var tfBrand: UITextField!
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var tfPrice: UITextField!
+    @IBOutlet weak var scGasType: UISegmentedControl!
+    @IBOutlet weak var btAddEdit: UIButton!
+    
+    // MARK: - Super Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - IBActions
+    @IBAction func addEdit(_ sender: UIButton) {
+    }
+}
---
+++ Carangas/Visualization/Controller/CarViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CarViewController.swift
+//  Carangas
+//
+//  Copyright © 2020 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class CarViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var lbBrand: UILabel!
+    @IBOutlet weak var lbGasType: UILabel!
+    @IBOutlet weak var lbPrice: UILabel!
+
+    // MARK: - Super Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
